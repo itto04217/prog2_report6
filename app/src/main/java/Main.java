803f8a2fd38600e5364557304271c49d9dee9735
Itto Kawamitsu <e245714@ie.u-ryukyu.ac.jp>
2025-01-05 11:01:32 +0900

    3

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -3,18 +3,19 @@ import jp.ac.uryukyu.ie.e245714.*;
 
 public class Main {
     public static void main(String[] args){
-        var drinks = new ArrayList<String>();
+        var drinks = new ArrayList<Drink>();
 
         Drink cola = new Drink("コーラ",180);
         Drink water = new Drink("水",130);
         Drink fanta = new Drink("ファンタ",150);
 
-        drinks.add(cola.getName());
-        drinks.add(water.getName());
-        drinks.add(fanta.getName());
-        
-        VendingMachine vendingMachine = new VendingMachine();
+        drinks.add(cola);
+        drinks.add(water);
+        drinks.add(fanta);
+
+        VendingMachine vendingMachine = new VendingMachine(20);
 
         vendingMachine.print(drinks);
+        vendingMachine.select(3,drinks);
     }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e245714/VendingMachine.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245714/VendingMachine.java
@@ -2,11 +2,31 @@ package jp.ac.uryukyu.ie.e245714;
 import java.util.ArrayList;
 
 public class VendingMachine {
-    public static <T> void print(ArrayList<T> drinks) {
+
+    int price;
+    public VendingMachine(int price){
+        this.price = price;
+        System.out.println("お金を"+price+"円,投入しました！");
+    }
+
+    public static <T> void print(ArrayList<Drink> drinks) {
         System.out.println("この自販機ではこれらの飲み物が買えます。");
         for(int i=0; i < drinks.size(); i++) {
-            System.out.print(i+1);
-            System.out.println(drinks.get(i));
+            System.out.print(i+1+",");
+            System.out.print(drinks.get(i).getName());
+            System.out.println(drinks.get(i).getPrice()+"円");
+        }
+    }
+
+    public void select(int i,ArrayList<Drink> drinks){
+        System.out.println(i+"番目の商品"+drinks.get(i-1).getName()+"を選択しました。");
+        if (price >= drinks.get(i-1).getPrice()){
+           System.out.print("購入できました！");
+           int oturi = price - drinks.get(i-1).getPrice();
+           System.out.println("お釣りは"+oturi+"円になります。取り忘れないようにしましょう。");
+        }else{
+            int tarinai = drinks.get(i-1).getPrice() - price ;
+            System.out.println("お金が"+tarinai+"円足りません。お疲れ様でした。");
         }
     }
 }
